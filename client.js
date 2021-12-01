b7ab96167c47720bac6f9a69839774652746e2e9
Fardosa Garad <fardosamgarad@gmail.com>
2021-12-01 14:48:16 -0500

    refactoring client server

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+      port: 50541
+  }); 
+  conn.on("connect", () => {
+console.log("Successfully connected to server")
+conn.write("Name: FMG")
+
+  });
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+module.exports = {
+  connect
+}
+
